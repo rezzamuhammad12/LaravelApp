c1e91bfb28d682a3d39d84d6bb4ef58635fae2c7
Rezza Mochamad <rezzamuhammad12@gmail.com>
2019-12-05 17:32:41 +0700

    Menambahkan field delete di tabel Students

--- app/Student.php
+++ app/Student.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Student extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['nama', 'nrp', 'email', 'jurusan'];
 }
